a45ac652c7716c800b65600d69e02b4953ee9810
adiyanai <adiyanai10@gmail.com>
2020-03-15 03:38:31 +0200

    Combine the login and signUp funcs to one func

--- lib/scoped-models/user.dart
+++ lib/scoped-models/user.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:http/http.dart' as http;
 import '../models/user.dart';
+import '../models/auth.dart';
 
 class UserModel extends Model {
   User _authenticatedUser;
@@ -12,11 +13,8 @@ class UserModel extends Model {
     return _isLoading;
   }
 
-  void login(String email, String password) {
-    _authenticatedUser = User(id: 'kndbhjnc', email: email, password: password);
-  }
-
-  Future<Map<String, dynamic>> signUp(String email, String password) async {
+  Future<Map<String, dynamic>> authenticate(String email, String password,
+      [AuthMode mode = AuthMode.Login]) async {
     _isLoading = true;
     notifyListeners();
     final Map<String, dynamic> authData = {
@@ -24,17 +22,35 @@ class UserModel extends Model {
       'password': password,
       'returnSecureToken': true
     };
-    final http.Response response = await http.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBePDkpa3WV4UVazs9tRi0WnicXHsj2Ui0',
-      body: json.encode(authData),
-      headers: {'Content-Type': 'application/json'},
-    );
+    http.Response response;
+    if (mode == AuthMode.Login) {
+      response = await http.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBePDkpa3WV4UVazs9tRi0WnicXHsj2Ui0',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'},
+      );
+    } else {
+      response = await http.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBePDkpa3WV4UVazs9tRi0WnicXHsj2Ui0',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'},
+      );
+    }
+
     final Map<String, dynamic> responseData = json.decode(response.body);
-    bool hasError =true;
+    bool hasError = true;
     String message = 'Something went worng';
     if (responseData.containsKey('idToken')) {
       hasError = false;
       message = 'Authenticaion succeeded!';
+      _authenticatedUser = User(
+          id: responseData['localId'],
+          email: email,
+          token: responseData['idToken']);
+    } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
+      message = 'דוא"ל לא קיים';
+    } else if (responseData['error']['message'] == 'INVALID_PASSWORD') {
+      message = 'סיסמה לא תקינה';
     } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
       message = 'דוא"ל זה כבר קיים!';
     }
