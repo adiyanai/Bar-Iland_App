260ad436e282df5632d2de3ff7f64ccd2099460a
adiyanai <adiyanai10@gmail.com>
2020-03-16 15:37:54 +0200

    Add ForgetPassword widget

---
+++ lib/pages/forget_password.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import '../scoped-models/main.dart';
+
+class ForgetPassword extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ForgetPasswordState();
+  }
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email = null;
+
+  void _submitForm(Function resetPassword) async {
+    print('here!!!!');
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    final Map<String, dynamic> successInformation = await resetPassword(_email);
+    if (successInformation['success']) {
+      print('success!!!');
+      Navigator.pop(context);
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: AlertDialog(
+              title: Text('אירעה שגיאה'),
+              content: Text(successInformation['message']),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('אישור'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  }
+
+  DecorationImage _buildBackgroungImage() {
+    return DecorationImage(
+      image: AssetImage('assets/background.jpg'),
+      fit: BoxFit.cover,
+      colorFilter: ColorFilter.mode(
+        Colors.black.withOpacity(0.55),
+        BlendMode.dstATop,
+      ),
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'דוא"ל',
+        suffixIcon: Icon(Icons.email),
+        filled: true,
+        fillColor: Colors.white70,
+      ),
+      keyboardType: TextInputType.emailAddress,
+      textAlign: TextAlign.right,
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                .hasMatch(value)) {
+          return 'הכנס כתובת דוא"ל תקינה';
+        }
+      },
+      onSaved: (String value) {
+        _email = value;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'שחזור סיסמה',
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: _buildBackgroungImage(),
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+            },
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/Bar_Iland_line.png',
+                    height: 200,
+                  ),
+                  _buildEmailTextField(),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ScopedModelDescendant<MainModel>(
+                    builder:
+                        (BuildContext context, Widget child, MainModel model) {
+                      return model.isLoading
+                          ? Center(child: CircularProgressIndicator())
+                          : Center(
+                              child: RaisedButton(
+                                color: Theme.of(context).primaryColor,
+                                child: Text(
+                                  'שחזר',
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  _submitForm(model.resetPassword);
+                                },
+                              ),
+                            );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
