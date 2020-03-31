68eba90c33919e7ebe4281b29f08092a4b455644
adiyanai <adiyanai10@gmail.com>
2020-03-31 15:30:46 +0300

    Add navigate to the events page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -73,152 +73,173 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: ListView(
-              children: <Widget>[
-                    Image.asset(
-                              'assets/Bar_Iland_line.png',
-                              height: 150,
-                              width: 30,
-                              color: Colors.black,
-                            ),
-                      SizedBox(height: 1),      
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children:[                  
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {}, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.info_outline ), // icon
-                                          Text("מידע על", style: TextStyle(fontSize: 14),textAlign: TextAlign.center), // text
-                                          Text("קורסים", style: TextStyle(fontSize: 14),textAlign: TextAlign.center,), // text
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {}, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.event), // icon
-                                          Text('אירועים ',
-                                          style: TextStyle(fontSize: 14),
-                                          textAlign: TextAlign.center,), // text
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {
-                                        Navigator.pushNamed(context, '/service_manager');
-                                      }, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.business), // icon
-                                          Text('שירותים באוניברסיטה',
-                                          style: TextStyle(fontSize: 13),
-                                          textAlign: TextAlign.center,)
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                        ],),
-                      SizedBox(height: 15),      
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children:[
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {}, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.find_in_page), // icon
-                                          Text('אבידות ומציאות ',
-                                          style: TextStyle(fontSize: 14),
-                                          textAlign: TextAlign.center,)                                ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {}, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.link), // icon
-                                          Text('קישורים חשובים',
-                                          style: TextStyle(fontSize: 14),
-                                          textAlign: TextAlign.center,)// text
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox.fromSize(
-                                size: Size(90, 90), // button width and height
-                                child: ClipOval(
-                                  child: Material(
-                                    color: Colors.lightBlue[200], // button color
-                                    child: InkWell(
-                                      splashColor: Colors.cyanAccent, // splash color
-                                      onTap: () {}, // button pressed
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(Icons.directions_bus), // icon
-                                          Text('תחבורה ושאטלים',
-                                          style: TextStyle(fontSize: 14),
-                                          textAlign: TextAlign.center,)                                ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                        ],),
-                      ],
-            ),
-          );  
+        children: <Widget>[
+          Image.asset(
+            'assets/Bar_Iland_line.png',
+            height: 150,
+            width: 30,
+            color: Colors.black,
+          ),
+          SizedBox(height: 1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {}, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.info_outline), // icon
+                          Text("מידע על",
+                              style: TextStyle(fontSize: 14),
+                              textAlign: TextAlign.center), // text
+                          Text(
+                            "קורסים",
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ), // text
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {
+                        Navigator.pushNamed(context, '/events');
+                      }, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.event), // icon
+                          Text(
+                            'אירועים ',
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ), // text
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {
+                        Navigator.pushNamed(context, '/service_manager');
+                      }, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.business), // icon
+                          Text(
+                            'שירותים באוניברסיטה',
+                            style: TextStyle(fontSize: 13),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {}, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.find_in_page), // icon
+                          Text(
+                            'אבידות ומציאות ',
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {}, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.link), // icon
+                          Text(
+                            'קישורים חשובים',
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          ) // text
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox.fromSize(
+                size: Size(90, 90), // button width and height
+                child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlue[200], // button color
+                    child: InkWell(
+                      splashColor: Colors.cyanAccent, // splash color
+                      onTap: () {}, // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.directions_bus), // icon
+                          Text(
+                            'תחבורה ושאטלים',
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
