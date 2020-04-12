5568583b62ca6c453fd9758efd5a5f739c62c123
adiyanai <adiyanai10@gmail.com>
2020-04-12 18:58:04 +0300

    Add: with EventsModel

--- lib/scoped-models/main.dart
+++ lib/scoped-models/main.dart
@@ -2,7 +2,8 @@ import 'package:scoped_model/scoped_model.dart';
 
 import './user.dart';
 import './services.dart';
+import './events.dart';
 
-class MainModel extends Model with UserModel, ConnectedServicesModel, ServicesModel,  UtilityModel {
+class MainModel extends Model with UserModel, ConnectedServicesModel, ServicesModel,  UtilityModel, EventsModel {
 
 }
