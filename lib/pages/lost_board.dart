1c7ada9ef85e6bfafdfdaf019337daa57ae2b84b
yamit.ct <yamit.ct@gmail.com>
2020-05-20 15:22:53 +0200

    Adding CircularProgressIndicator when the data is loaded. Adding lost - dialog for contact details.

--- lib/pages/lost_board.dart
+++ lib/pages/lost_board.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:bar_iland_app/scoped-models/main.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:grouped_buttons/grouped_buttons.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LostBoard extends StatefulWidget {
   final MainModel model;
@@ -15,12 +17,12 @@ class LostBoard extends StatefulWidget {
 
 class _LostBoardState extends State<LostBoard> {
   String _selLostFoundType = "";
-  String _selLostFoundDescription = '';
+  String _selLostFoundDescription = "";
+  List<String> _selOptionalLostLocations = [];
+
   @override
   void initState() {
     super.initState();
-    widget.model.fetchLostFoundTypes();
-    widget.model.fetchServicesLocations();
   }
 
   @override
@@ -47,98 +49,114 @@ class _LostBoardState extends State<LostBoard> {
         ]),
       ]),
     );
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/lost_found.jpg'),
-            fit: BoxFit.fill,
-            colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.7),
-              BlendMode.dstATop,
-            ),
-          ),
-        ),
-        child: Stack(children: [
-          Container(
-            height: 40,
-            margin: EdgeInsets.symmetric(vertical: 25, horizontal: 8),
+
+    return ScopedModelDescendant<MainModel>(
+        builder: (BuildContext context, Widget child, MainModel model) {
+      Widget content;
+      if (!model.isLostFoundLoading) {
+        content = Directionality(
+          textDirection: TextDirection.rtl,
+          child: Container(
             decoration: BoxDecoration(
-              color: Color.fromRGBO(210, 245, 250, 0.7),
-              borderRadius: BorderRadius.circular(5),
-              border: Border.all(
-                color: Colors.blue[200],
+              image: DecorationImage(
+                image: AssetImage('assets/lost_found.jpg'),
+                fit: BoxFit.fill,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.7),
+                  BlendMode.dstATop,
+                ),
               ),
             ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  RaisedButton.icon(
-                    icon: Icon(MaterialCommunityIcons.filter),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    label: Text("סינון"),
-                    onPressed: () {},
+            child: Stack(children: [
+              Container(
+                height: 40,
+                margin: EdgeInsets.symmetric(vertical: 25, horizontal: 8),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(210, 245, 250, 0.7),
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: Colors.blue[200],
                   ),
-                  RaisedButton.icon(
-                    icon: Icon(Icons.add),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    label: Text("הוספת אבידה"),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return _lostFoundTypeDialog();
+                ),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      RaisedButton.icon(
+                        icon: Icon(MaterialCommunityIcons.filter),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        label: Text("סינון"),
+                        onPressed: () {},
+                      ),
+                      RaisedButton.icon(
+                        icon: Icon(Icons.add),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        label: Text("הוספת אבידה"),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return _lostFoundTypeDialog();
+                            },
+                          );
                         },
-                      );
-                    },
-                  ),
-                ]),
+                      ),
+                    ]),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 80),
+                child: ListView.builder(
+                  itemCount: losts.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      alignment: Alignment.topRight,
+                      margin: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(210, 245, 250, 0.7),
+                        border: Border.all(
+                          color: Colors.blue[200],
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: losts[index],
+                    );
+                  },
+                ),
+              ),
+            ]),
           ),
-          Container(
-            padding: EdgeInsets.only(top: 80),
-            child: ListView.builder(
-              itemCount: losts.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  alignment: Alignment.topRight,
-                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(210, 245, 250, 0.7),
-                    border: Border.all(
-                      color: Colors.blue[200],
-                    ),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: losts[index],
-                );
-              },
-            ),
-          ),
-        ]),
-      ),
-    );
+        );
+      } else if (model.isLostFoundLoading) {
+        content = Center(child: CircularProgressIndicator());
+      }
+      return content;
+    });
   }
 
   Widget _lostFoundTypeDialog() {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: AlertDialog(
-        title: Text('בחר/י סוג אבידה:'),
+        title: Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Text('בחר/י סוג אבידה:')),
         content: SingleChildScrollView(
+            child: Container(
+          alignment: Alignment.topRight,
           child: RadioButtonGroup(
             labels: widget.model.LostFoundTypes,
             onSelected: (String selected) => {_selLostFoundType = selected},
           ),
-        ),
+        )),
         actions: <Widget>[
           FlatButton(
             child: Text('הבא'),
             onPressed: () {
-              Navigator.of(context).pop();
-              _lostFoundDescriptionDialog(context);
+              if (_selLostFoundType != "") {
+                Navigator.of(context).pop();
+                _lostFoundDescriptionDialog(context);
+              }
             },
           ),
           FlatButton(
@@ -197,9 +215,9 @@ class _LostBoardState extends State<LostBoard> {
   }
 
   Future<String> _lostFoundLocationDialog(BuildContext context) async {
-    List<String> locations = [];
+    List<String> lostFoundLocations = [];
     widget.model.Locations.forEach((location) {
-      locations.add(location.Number + " - " + location.Name);
+      lostFoundLocations.add(location.Number + " - " + location.Name);
     });
     return showDialog<String>(
       context: context,
@@ -211,10 +229,10 @@ class _LostBoardState extends State<LostBoard> {
             child: Stack(children: [
               Container(
                 height: 600,
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 1),
+                padding: EdgeInsets.fromLTRB(0, 20, 15, 10),
                 child: Text(
-                  "מקומות משוערים להימצאות האבידה:",
-                  style: TextStyle(fontSize: 18),
+                  "היכן ייתכן שהאבידה נמצאת?",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
               Container(
@@ -229,6 +247,7 @@ class _LostBoardState extends State<LostBoard> {
                         ),
                         onPressed: () {
                           Navigator.of(context).pop();
+                          _lostFoundContactDialog(context);
                         },
                       ),
                       FlatButton(
@@ -247,8 +266,10 @@ class _LostBoardState extends State<LostBoard> {
                 height: 500,
                 child: ListView(children: [
                   CheckboxGroup(
-                    labels: locations,
-                    onSelected: (List<String> checked) => {},
+                    padding: EdgeInsets.only(right: 10),
+                    labels: lostFoundLocations,
+                    onSelected: (List<String> checked) =>
+                        {_selOptionalLostLocations = checked},
                     itemBuilder: (Checkbox cb, Text txt, int i) {
                       Text text = Text(
                         txt.data,
@@ -267,6 +288,55 @@ class _LostBoardState extends State<LostBoard> {
                 ]),
               ),
             ]),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<String> _lostFoundContactDialog(BuildContext context) async {
+    return showDialog<String>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: AlertDialog(
+            title: Text('טלפון ליצירת קשר:'),
+            content: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(10),
+                    ],
+                    decoration: new InputDecoration(icon: Icon(Icons.phone)),
+                    onChanged: (value) {
+                      _selLostFoundDescription = value;
+                    },
+                  ),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('הבא'),
+                onPressed: () {
+                  if (_selLostFoundDescription != "" &&
+                      _selLostFoundDescription.length >= 9) {
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+              FlatButton(
+                child: Text('ביטול'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
           ),
         );
       },
