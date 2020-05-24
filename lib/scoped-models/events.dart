28a97903fcd939b07be62e7945fd0998a903f1b0
adiyanai <adiyanai10@gmail.com>
2020-05-24 10:19:21 +0300

    Add event with http that include auth=user.Token

--- lib/scoped-models/events.dart
+++ lib/scoped-models/events.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/event.dart';
 import '../models/event_location.dart';
+import './main.dart';
 
 class EventsModel extends Model {
   final databaseURL = 'https://bar-iland-app.firebaseio.com/events';
@@ -63,7 +64,7 @@ class EventsModel extends Model {
   }
 
   Future<bool> addEvent(DateTime date, String time, String location,
-      String eventType, String eventDescription) async {
+      String eventType, String eventDescription, MainModel model) async {
     _isAddEventLoading = true;
     notifyListeners();
     final Map<String, dynamic> eventData = {
@@ -76,14 +77,13 @@ class EventsModel extends Model {
 
     try {
       final http.Response response = await http
-          .post(databaseURL + '/eventsData.json', body: json.encode(eventData));
+          .post(databaseURL + '/eventsData.json' + '?auth=${model.user.Token}', body: json.encode(eventData));
 
       if (response.statusCode != 200 && response.statusCode != 201) {
         _isAddEventLoading = false;
         notifyListeners();
         return false;
       }
-      print('end send event');
       final Map<String, dynamic> responseData = json.decode(response.body);
       final Event newEvent = Event(
           id: responseData['name'],
@@ -93,10 +93,8 @@ class EventsModel extends Model {
           eventType: eventType,
           eventDescription: eventDescription);
       _events.add(newEvent);
-      print('add event to list');
       _selEventId = responseData['name'];
       _isAddEventLoading = false;
-      print('isEventsLoading is false');
       notifyListeners();
       return true;
     } catch (error) {
