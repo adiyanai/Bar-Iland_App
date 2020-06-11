c9109a30f66c4aefdd6547a0bd1d3cefb117a6e6
adiyanai <adiyanai10@gmail.com>
2020-06-11 10:59:50 +0300

    Add BusesShuttlesManager

---
+++ lib/managers/buses_shuttles_manager.dart
@@ -0,0 +1,55 @@
+import 'package:bar_iland_app/scoped-models/main.dart';
+import 'package:flutter/material.dart';
+import '../pages/buses_by_city.dart';
+
+class BusesShuttlesManager extends StatelessWidget {
+  final MainModel model;
+  BusesShuttlesManager(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    model.fetchBuses();
+    model.fetchStations();
+    return DefaultTabController(
+      length: 4,
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('תחבורה ושאטלים'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.directions_railway),
+                  text: 'שאטלים',
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bus),
+                  text: 'תחנות',
+                ),
+                Tab(
+                  icon: Icon(Icons.location_city),
+                  text: 'קו לפי עיר',
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_car),
+                  text: 'חניות',
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(),
+              Container(),
+              BusesByCity(model),
+              Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
