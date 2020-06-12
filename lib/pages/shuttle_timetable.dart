a2e4724e0e8986ae862718e99a9d001ea92be0c3
adiyanai <adiyanai10@gmail.com>
2020-06-12 15:01:24 +0300

    Add ShuttleTimetable

---
+++ lib/pages/shuttle_timetable.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ShuttleTimetable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'לוח זמנים',
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/shuttle_timetable.png'),
+              fit: BoxFit.contain,
+              /*colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.55),
+                BlendMode.dstATop,
+              ),*/
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
