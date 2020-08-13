dc44f65d865aebad90e8e748be9101a9d11a91ea
einavcohen <einavcohen006@gmail.com>
2020-08-13 20:55:00 +0300

    Adding comments in links.dart page

--- lib/scoped-models/links.dart
+++ lib/scoped-models/links.dart
@@ -13,153 +13,157 @@ Map<String,SplayTreeMap<String,List<Degree>>> _fetchedData = {};
 bool _isLinksLoading = false;
 
 List<Degree> get allDegrees{
-  return List.from(_degrees);
+return List.from(_degrees);
 }
 
 List<String> get allFaculties{
-  return List.from(_faculties);
+return List.from(_faculties);
 }
 
 Map<String,SplayTreeMap<String,List<Degree>>> get allData{
-  return (_fetchedData);
+return (_fetchedData);
 }
 
 bool get isLinksLoading {
-    return _isLinksLoading;
+return _isLinksLoading;
 }
 
+// fetch all the data of the degrees from the database
 Future<Null> fetchAllDegrees(){
   _isLinksLoading = true;
   return http.get('https://bar-iland-app.firebaseio.com/importantLinks.json')
   .then<Null>((http.Response response){
-    final List<Degree> fetchedDegreesList = [];
-    final Map<String, dynamic> degreesData = json.decode(response.body);
-    if (degreesData == null) {
-        _isLinksLoading = false;
-        notifyListeners();
-        return;
-      }
-    degreesData.forEach((String facultyType, dynamic facultyData) {
-      if (int.tryParse(facultyType) == null){
-        Degree degree;
-        facultyData.forEach((String idDegree , dynamic dataDegree){
-      degree = Degree(
-        id: idDegree,
-        degreeType :dataDegree['degree'],
-        name: dataDegree['name'],
-        url: dataDegree['url']);
-      fetchedDegreesList.add(degree);
-        });
-      }
-    });
-    _degrees = fetchedDegreesList;
+  final List<Degree> fetchedDegreesList = [];
+  final Map<String, dynamic> degreesData = json.decode(response.body);
+  if (degreesData == null) {
     _isLinksLoading = false;
     notifyListeners();
-   }).catchError((error) {
-      _isLinksLoading = false;
-      notifyListeners();
-      return;
+    return;
+  }
+  degreesData.forEach((String facultyType, dynamic facultyData) {
+  if (int.tryParse(facultyType) == null){
+    Degree degree;
+    facultyData.forEach((String idDegree , dynamic dataDegree){
+    degree = Degree(
+    id: idDegree,
+    degreeType :dataDegree['degree'],
+    name: dataDegree['name'],
+    url: dataDegree['url']);
+    fetchedDegreesList.add(degree);
     });
- }
- 
+  }
+  });
+  _degrees = fetchedDegreesList;
+  _isLinksLoading = false;
+  notifyListeners();
+  }).catchError((error) {
+  _isLinksLoading = false;
+  notifyListeners();
+  return;
+  });
+}
+
+// fetch all the data of the facultues from the database
 Future<Null> fetchAllFaculties(){
   _isLinksLoading = true;
   return http.get('https://bar-iland-app.firebaseio.com/importantLinks.json')
   .then<Null>((http.Response response){
-    final List<String> fetchedFacultiesList = [];
-    final Map<String, dynamic> facultiesData = json.decode(response.body);
-    if (facultiesData == null) {
-        _isLinksLoading = false;
-        notifyListeners();
-        return;
-      }
-    facultiesData.forEach((String facultyType, dynamic facultyData) {
-      fetchedFacultiesList.add(facultyType);
-    });
-    _faculties = fetchedFacultiesList;
+  final List<String> fetchedFacultiesList = [];
+  final Map<String, dynamic> facultiesData = json.decode(response.body);
+  if (facultiesData == null) {
     _isLinksLoading = false;
     notifyListeners();
-  }).catchError((error) {
-      _isLinksLoading = false;
-      notifyListeners();
-      return;
+    return;
+  }
+  facultiesData.forEach((String facultyType, dynamic facultyData) {
+    fetchedFacultiesList.add(facultyType);
     });
-  }
+  _faculties = fetchedFacultiesList;
+  _isLinksLoading = false;
+  notifyListeners();
+  }).catchError((error) {
+  _isLinksLoading = false;
+  notifyListeners();
+  return;
+  });
+}
 
+// convert faculties names to the relevat types
 String convertFacultyType(String facultyName){
-      if(facultyName == 'engineering'){
-        return 'הפקולטה להנדסה';
-      }
-      else if(facultyName == 'exactSciences'){
-        return 'הפקולטה למדעים מדויקים';
-      } 
-      else if(facultyName == 'general'){
-        return 'מאגרי מידע נוספים';
-      } 
-      else if(facultyName == 'humanities'){
-        return 'הפקולטה למדעי הרוח';
-      } 
-      else if(facultyName == 'interdisciplinaryStudies'){
-        return 'לימודים בין תחומיים';
-      } 
-      else if(facultyName == 'jewishStudies'){
-        return 'הפקולטה למדעי היהדות';
-      } 
-      else if(facultyName == 'lifeScience'){
-        return 'הפקולטה למדעי החיים';
-      } 
-      else if(facultyName == 'socialSciences'){
-        return 'הפקולטה למדעי החברה';
-      }
-    else{
-      return 'invalid faculty name';
-    }  
+  if(facultyName == 'engineering'){
+  return 'הפקולטה להנדסה';
   }
+  else if(facultyName == 'exactSciences'){
+  return 'הפקולטה למדעים מדויקים';
+  } 
+  else if(facultyName == 'general'){
+  return 'מאגרי מידע נוספים';
+  } 
+  else if(facultyName == 'humanities'){
+  return 'הפקולטה למדעי הרוח';
+  } 
+  else if(facultyName == 'interdisciplinaryStudies'){
+  return 'לימודים בין תחומיים';
+  } 
+  else if(facultyName == 'jewishStudies'){
+  return 'הפקולטה למדעי היהדות';
+  } 
+  else if(facultyName == 'lifeScience'){
+  return 'הפקולטה למדעי החיים';
+  } 
+  else if(facultyName == 'socialSciences'){
+  return 'הפקולטה למדעי החברה';
+  }
+  else{
+  return 'invalid faculty name';
+  }  
+}
 
+// fetch all data from importantLinks folder in the database 
 Future<Null> fetchAll(){
   _isLinksLoading = true;
   return http.get('https://bar-iland-app.firebaseio.com/importantLinks.json')
   .then<Null>((http.Response response){
-    Map<String,SplayTreeMap<String,List<Degree>>> facultiesToDegrees = {};
-    Map<String, dynamic> allData = json.decode(response.body);
-    if (allData == null) {
-        _isLinksLoading = false;
-        notifyListeners();
-        return;
-      }
-    allData.forEach((String facultyType, dynamic facultyDataMap){
-      if (facultyType == null){
-        _isLinksLoading = false;
-        notifyListeners();
-        return;
-      }   
-        String convertedFacultyType = convertFacultyType(facultyType); 
-         SplayTreeMap<String,List<Degree>> degrees_all_data = SplayTreeMap<String,List<Degree>>();
-
-        facultyDataMap.forEach((String facultyId, dynamic degressInFaculty){
-            Degree degree = Degree(
-                degreeType :degressInFaculty['degree'],
-                name: degressInFaculty['name'],
-                url: degressInFaculty['url']);
-            if(degrees_all_data[degree.degreeType] == null){
-              degrees_all_data[degree.degreeType] =[];
-              degrees_all_data[degree.degreeType].add(degree);
-              facultiesToDegrees[convertedFacultyType] = degrees_all_data;
-            }else{
-              degrees_all_data[degree.degreeType].add(degree);
-              facultiesToDegrees[convertedFacultyType] = degrees_all_data;
-            }
-          });
-       });;
-
-    _fetchedData = facultiesToDegrees;    
-    //print(_fetchedData);
+  Map<String,SplayTreeMap<String,List<Degree>>> facultiesToDegrees = {};
+  Map<String, dynamic> allData = json.decode(response.body);
+  if (allData == null) {
     _isLinksLoading = false;
     notifyListeners();
+    return;
+  }
+  allData.forEach((String facultyType, dynamic facultyDataMap){
+  if (facultyType == null){
+    _isLinksLoading = false;
+    notifyListeners();
+    return;
+  }   
+String convertedFacultyType = convertFacultyType(facultyType); 
+  SplayTreeMap<String,List<Degree>> degrees_all_data = SplayTreeMap<String,List<Degree>>();
+
+facultyDataMap.forEach((String facultyId, dynamic degressInFaculty){
+    Degree degree = Degree(
+        degreeType :degressInFaculty['degree'],
+        name: degressInFaculty['name'],
+        url: degressInFaculty['url']);
+    if(degrees_all_data[degree.degreeType] == null){
+      degrees_all_data[degree.degreeType] =[];
+      degrees_all_data[degree.degreeType].add(degree);
+      facultiesToDegrees[convertedFacultyType] = degrees_all_data;
+    }else{
+      degrees_all_data[degree.degreeType].add(degree);
+      facultiesToDegrees[convertedFacultyType] = degrees_all_data;
+    }
+  });
+});;
+
+_fetchedData = facultiesToDegrees;    
+  //print(_fetchedData);
+  _isLinksLoading = false;
+  notifyListeners();
   }).catchError((error) {
-      _isLinksLoading = false;
-      notifyListeners();
-      return;
-    }); 
-}
+  _isLinksLoading = false;
+  notifyListeners();
+  return;
+  }); 
+  }
 }
